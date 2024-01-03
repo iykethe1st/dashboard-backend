b2b4e833bf4e79f9f8f02c63400b28d8dd5ff3b8
Ikenna <iykethe1st@gmail.com>
2024-01-03 09:45:50 +0100

    added auth custom decorator

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from "@nestjs/common";
+import { User } from "@prisma/client";
+import { GetUser } from "src/auth/decorator";
+import { JwtGuard } from "src/auth/guard";
 
-@Controller('user')
-export class UserController {}
+@Controller("users")
+export class UserController {
+  @UseGuards(JwtGuard)
+  @Get("me")
+  getMe(@GetUser() user: User) {
+    return user;
+  }
+}
